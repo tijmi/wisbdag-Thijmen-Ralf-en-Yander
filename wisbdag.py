0a3611f2674c4faa86fd8d55d0a29de0aa8063d9
tijmi <thdegroote18@gmail.com>
2022-11-11 10:50:59 +0100

    Initial commit

---
+++ wisbdag.py
@@ -0,0 +1,26 @@
+import math
+x=0
+aantalpi=0
+aantalleeg=0
+clostest1=0
+aantalpibijg=0
+aantalleegbijg=0
+hoeveelverschil=3
+
+
+while True:
+  x+=math.pi
+  aantalpi+=1
+  if x>5:
+    aantalleeg+=1
+    x=x-5
+    if abs(x-1)<hoeveelverschil:
+        hoeveelverschil=abs(x-1)
+        clostest1=x
+        aantalleegbijg=aantalleeg
+        aantalpibijg=aantalpi
+        print("aantal kannen met pi gevuld= "+ str(aantalpibijg))
+        print("aantal keer kan 5 liter leeggegooid= " + str(aantalleegbijg))
+        print("getal =" + str(clostest1))
+        print(f"het zit {hoeveelverschil} van 1 verwijderd")
+        print("")
